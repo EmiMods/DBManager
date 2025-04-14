39bd76692f40e0277f0b1314d61a527ee40ff01d
EmiMods <emily9716s@outlook.com>
2025-04-13 23:15:56 -0500

    Direction shift to accommodate query building/execution

--- DBrowser/Main.lua
+++ DBrowser/Main.lua
@@ -1,4 +1,4 @@
-scriptTitle = "Aurora Database Browser"
+scriptTitle = "Aurora Database Manager"
 scriptAuthor = "EmiMods"
 scriptVersion = 1.0
 scriptDescription = "Browse Aurora's content and settings databases"
@@ -33,11 +33,185 @@ Tables = {
     { {TableSubitemPadding .. "UserSettings"}, {"Id","Name","Value","ProfileId"} }
 }
 
+CurrentQuery = ""
+
+QueryBuilderMenu = {
+    "Show SQL Syntax",
+    "Show Keyboard",
+    "Show Column By Table",
+    "Delete Last Entry",
+    "",
+    "Clear Query",
+    "Quit",
+    "Main Menu",
+    "",
+    "Execute Query"
+}
+
+SqlSyntax = {
+    "SELECT",
+    "SELECT *",
+    "UPDATE",
+    "INSERT",
+    "INTO",
+    "DELETE",
+    "DISTINCT",
+    "AS",
+    "SET",
+    "FROM",
+    "WHERE",
+    "AND",
+    "OR",
+    "ANY",
+    "ALL",
+    "NOT",
+    "LIKE",
+    "CASE",
+    "WHEN",
+    "THEN",
+    "ELSE",
+    "END",
+    "JOIN",
+    "LEFT JOIN",
+    "RIGHT JOIN",
+    "CROSS JOIN",
+    "UNION",
+    "ON",
+    "DO",
+    "BETWEEN",
+    "IN",
+    "ORDER BY",
+    "GROUP BY",
+    "HAVING",
+    "ASC",
+    "DESC",
+    "LIMIT",
+    "NULL",
+    "NOTHING",
+    "EXCEPT",
+    "VALUES(",
+    "CONFLICT(",
+    "SUM(",
+    "AVG(",
+    "MAX(",
+    "MIN(",
+    "GROUP_CONCAT(",
+    "",
+}
+
+Keyboard = {
+    "SPACE",
+    ",",
+    "'",
+    "(",
+    ")",
+    ".",
+    "=",
+    "==",
+    "+",
+    "-",
+    "*",
+    "/",
+    "%",
+    ">",
+    "<",
+    ">=",
+    ">=",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "\"",
+    "-",
+    "_",
+    ":",
+    ";",
+    "{",
+    "}",
+    "$",
+    "@",
+    "!",
+    "#",
+    "^",
+    "\\";
+    "~",
+    "`",
+    "|",
+    "&",
+    ""
+}
+
+MainMenu = {
+    "Manually Query Databases",
+    "View Table (All Entries)"
+}
+
 -- Main entry point to script
 function main()
-    local selectedTableKey = PromptTableSelect()
+    local selectedMenuOption = ShowMainMenu();
 
-    ShowTable(selectedTableKey)
+    local selectedTableKey =   PromptTableSelect()
+    if selectedMenuOption == 1 then
+        ShowTable(selectedTableKey)
+    elseif selectedMenuOption == 2 then
+        ShowQueryBuilder()
+    end
+end
+
+-- Menu for user to build and execute queries
+function ShowQueryBuilder()
+    local dialogBox = Script.ShowPopupList("Query Builder", "Error", QueryBuilderMenu)
+    local selected = dialogBox.Selected.Key
+
+    if selected == QueryBuilderMenu[1] then     -- "Show SQL Syntax"
+    elseif selected == QueryBuilderMenu[2] then -- "Show Keyboard"
+    elseif selected == QueryBuilderMenu[3] then -- "Show Column By Table"
+    elseif selected == QueryBuilderMenu[4] then -- "Delete Last Entry"
+    elseif selected == QueryBuilderMenu[5] then -- ""
+    elseif selected == QueryBuilderMenu[6] then -- "Clear Query"
+    elseif selected == QueryBuilderMenu[7] then -- "Quit"
+    elseif selected == QueryBuilderMenu[8] then -- "Main Menu"
+    elseif selected == QueryBuilderMenu[9] then -- ""
+    elseif selected == QueryBuilderMenu[10] then -- "Execute Query"
+        
+    end
+end
+
+function ShowMainMenu()
+    local dialogBox = Script.ShowPopupList(scriptTitle, "Error", MainMenu)
+    return dialogBox.Selected.Key
 end
 
 -- Prompts user to select a table for browsing and returns selected index
